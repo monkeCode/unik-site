5ea027ab6885cb3b6aeffe697d7c3791d3353f82
Georgy <georgy.tertychniy@yandex.ru>
2024-01-29 16:07:03 +0300

    PROD env variable for server

--- server/wsgi-entrypoint.sh
+++ server/wsgi-entrypoint.sh
@@ -14,6 +14,9 @@ done
 
 python manage.py collectstatic --noinput
 
-#gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 1 --log-level debug
 
-python manage.py runserver 0.0.0.0:8000
+if [ "$PROD" = "true" ]; then
+    gunicorn unikSite.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 1 --log-level debug
+else
+    python manage.py runserver 0.0.0.0:8000
+    fi
